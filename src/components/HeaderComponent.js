d3f2ad86b809c3a6d82bef8ac1d13f5cdc36ce52
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-07-17 14:08:27 +0530

    header and footer

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+        <React.Fragment>
+        <Navbar dark>
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Jumbotron>
+            <div className="container">
+                <div className="row row-header">
+                    <div className="col-12 col-sm-6">
+                        <h1>Ristorante Con Fusion</h1>
+                        <p>We take inspiration from the world's best cuisines, and create a unique fusion experience. our lipsmacking creations will tickle your culinary senses!</p>
+                    </div>
+                </div>                
+            </div>
+        </Jumbotron>
+        </React.Fragment>
+         );
+    }
+}
+ 
+export default Header;
