1f2732a9f9a76dd123abcea679328f31c1a994b9
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-07-20 21:16:42 +0530

    react-redux-form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
-import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import About from './AboutComponent';
 import {connect} from 'react-redux';
 
