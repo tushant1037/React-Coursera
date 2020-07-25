1dca752a15a5c3812374d964fda6116a7d03f2a4
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-07-25 23:24:28 +0530

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbIte
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+
 
 
 function RenderComments({comments, addComment, dishId}) {
@@ -41,7 +43,7 @@ function RenderComments({comments, addComment, dishId}) {
             return (
                 <div>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl+ dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
